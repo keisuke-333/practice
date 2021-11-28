12f55ab1477eb3f9fba3baf701850966a4735705
keisuke-333 <keikubo.999@gmail.com>
2021-11-28 16:16:54 +0900

    init

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import { Box } from "@chakra-ui/react"
+
+const Home = () => {
+  return (
+    <>
+      <Box p={10} bg="green" color="white">Hello</Box>
+      <p className="text-6xl font-light">Hello</p>
+    </>
+  )
+}
+
+export default Home
