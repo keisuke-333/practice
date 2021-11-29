ccfc2befd14dea21e9581b18aad62b69356e9cba
keisuke-333 <keikubo.999@gmail.com>
2021-11-29 12:53:56 +0900

    inline_styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,29 +1,11 @@
-import { useCallback, useState, } from 'react'
-import { Box, Button, Input } from '@chakra-ui/react'
-import ChildArea from '../components/ChildArea'
+import { Box } from '@chakra-ui/react'
+import { InlineStyle } from '../components/InlineStyle'
 
 const Home = () => {
-  console.log('Home')
-
-  const [text, setText] = useState('')
-  const [open, setOpen] = useState(false)
-
-  const onChangeText = (e) => setText(e.target.value)
-
-  const onClickOpen = () => setOpen(bool => !bool)
-
-  const onClickClose = useCallback(() => setOpen(false), [setOpen])
   
   return (
     <Box textAlign={'center'}>
-      <Input
-        value={text}
-        onChange={onChangeText}
-        w={100}
-        mr={1}
-      ></Input>
-      <Button onClick={onClickOpen}>表示</Button>
-      <ChildArea open={open} onClickClose={onClickClose}></ChildArea>
+      <InlineStyle />
     </Box>
   )
 }
