00b8f5ad280018848f86180de3a8b5c87efe7bd9
keisuke-333 <keikubo.999@gmail.com>
2021-12-05 16:58:58 +0900

    カスタムフックの呼び出し

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,10 @@
-import axios from "axios"
-import { useState } from "react"
 import { UserCard } from "./components/UserCard"
-import { user } from "./types/api/user"
-import { userProfile } from "./types/userProfile"
+import { useAllUsers } from "./hooks/useAllUsers"
 
 const App = () => {
-  const [userProfiles, setUserProfiles] = useState<Array<userProfile>>([])
-  const [loading, setLoading] = useState(false)
-  const [error, setError] = useState(false)
+  const { getUsers, userProfiles, loading, error } = useAllUsers()
 
-  const onClickFetchUser = () => {
-    setLoading(true)
-    axios.get<Array<user>>('https://jsonplaceholder.typicode.com/users').then((res) => {
-      const data = res.data.map((user) => ({
-        id: user.id,
-        name: `${user.name}(${user.username})`,
-        email: user.email,
-        address: `${user.address.city}${user.address.suite}${user.address.street}`
-      }))
-      setUserProfiles(data)
-    }).catch((e) => {
-      setError(true)
-    }).finally(() => {
-      setLoading(false)
-    })
-  }
+  const onClickFetchUser = () => getUsers()
 
   return (
     <>
