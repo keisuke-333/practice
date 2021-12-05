f9c9e9d9c9dc2989df9cce9645d0e8c58da647cc
keisuke-333 <keikubo.999@gmail.com>
2021-12-05 16:18:32 +0900

    カスタムフックを使わないデータ取得例

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,45 @@
+import axios from "axios"
+import { useState } from "react"
+import { UserCard } from "./components/UserCard"
+import { user } from "./types/api/user"
+import { userProfile } from "./types/userProfile"
+
 const App = () => {
+  const [userProfiles, setUserProfiles] = useState<Array<userProfile>>([])
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(false)
+
+  const onClickFetchUser = () => {
+    setLoading(true)
+    axios.get<Array<user>>('https://jsonplaceholder.typicode.com/users').then((res) => {
+      const data = res.data.map((user) => ({
+        id: user.id,
+        name: `${user.name}(${user.username})`,
+        email: user.email,
+        address: `${user.address.city}${user.address.suite}${user.address.street}`
+      }))
+      setUserProfiles(data)
+    }).catch((e) => {
+      setError(true)
+    }).finally(() => {
+      setLoading(false)
+    })
+  }
+
   return (
     <>
-      <p>テストテストテスト</p>
+      <button onClick={onClickFetchUser}>データ取得</button>
+      {error ? (
+        <p style={{ color: 'red' }}>データ取得に失敗しました。</p>
+      ) : loading ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          {userProfiles.map((user) => (
+            <UserCard key={user.id} user={user}/>
+          ))}
+        </>
+      )}
     </>
   )
 }
