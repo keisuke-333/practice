7b7b1eabab2be1dcd9ff44340d2f1720b9502fee
keisuke-333 <keikubo.999@gmail.com>
2021-12-06 14:15:38 +0900

    Chakra UI導入

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,10 @@
-import { UserCard } from "./components/UserCard"
-import { useAllUsers } from "./hooks/useAllUsers"
+import { Button, ChakraProvider } from "@chakra-ui/react"
 
 const App = () => {
-  const { getUsers, userProfiles, loading, error } = useAllUsers()
-
-  const onClickFetchUser = () => getUsers()
-
   return (
-    <>
-      <button onClick={onClickFetchUser}>データ取得</button>
-      {error ? (
-        <p style={{ color: 'red' }}>データ取得に失敗しました。</p>
-      ) : loading ? (
-        <p>Loading...</p>
-      ) : (
-        <>
-          {userProfiles.map((user) => (
-            <UserCard key={user.id} user={user}/>
-          ))}
-        </>
-      )}
-    </>
+    <ChakraProvider>
+      <Button colorScheme="teal">ボタン</Button>
+    </ChakraProvider>
   )
 }
 
