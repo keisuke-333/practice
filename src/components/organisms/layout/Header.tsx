112b8e20f0840b926769836f368aa6b5ffb3bfa3
keisuke-333 <keikubo.999@gmail.com>
2021-12-06 17:35:07 +0900

    ヘッダーをコンポーネント分割

--- src/components/organisms/layout/Header.tsx
+++ src/components/organisms/layout/Header.tsx
@@ -2,17 +2,13 @@ import { memo, VFC } from 'react'
 import {
   Flex,
   Heading,
-  Button,
   Link,
   Box,
-  IconButton,
-  Drawer,
-  DrawerOverlay,
-  DrawerContent,
-  DrawerBody,
   useDisclosure
 } from '@chakra-ui/react'
-import { HamburgerIcon } from '@chakra-ui/icons'
+
+import { MenuIconButton } from '../../atoms/button/MenuIconButton'
+import { MenuDrawer } from '../../molecules/MenuDrawer'
 
 export const Header: VFC = memo(() => {
   const { isOpen, onOpen, onClose } = useDisclosure()
@@ -51,31 +47,12 @@ export const Header: VFC = memo(() => {
           </Box>
           <Link>設定</Link>
         </Flex>
-        <IconButton
-          aria-label="メニューボタン"
-          icon={<HamburgerIcon/>}
-          size="sm"
-          variant="unstyled"
-          display={{ base: "block", md: "none" }}
-          onClick={onOpen}
-        />
+        <MenuIconButton onOpen={onOpen} />
       </Flex>
-      <Drawer
-        placement="left"
-        size="xs"
+      <MenuDrawer
         onClose={onClose}
         isOpen={isOpen}
-      >
-        <DrawerOverlay>
-          <DrawerContent>
-            <DrawerBody p={0} bg="gray.100">
-              <Button w="100%">Top</Button>
-              <Button w="100%">ユーザー一覧</Button>
-              <Button w="100%">設定</Button>
-            </DrawerBody>
-          </DrawerContent>
-        </DrawerOverlay>
-      </Drawer>
+      />
     </>
   )
 })
