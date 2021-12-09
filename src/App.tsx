2ca6fe9845dfb5b7a381ba627da921a2c43ef044
keisuke-333 <keikubo.999@gmail.com>
2021-12-10 01:25:37 +0900

    環境構築

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,8 @@
-import { ChakraProvider } from "@chakra-ui/react"
-import { BrowserRouter } from "react-router-dom"
-import { Router } from "./router/Router"
-
-import theme from "./theme/theme"
-
 const App = () => {
   return (
-    <ChakraProvider theme={theme}>
-      <BrowserRouter>
-        <Router />
-      </BrowserRouter>
-    </ChakraProvider>
+    <div>
+      テスト
+    </div>
   )
 }
 
