2999fb3bb475e8e4ae27ce2c8f405fb7bab05ff6
keisuke-333 <keikubo.999@gmail.com>
2021-12-10 17:58:58 +0900

    GraphQLクライアントをセットアップ

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,41 @@
+import { useState, useEffect } from 'react'
+import gql from 'graphql-tag'
+
+import client from './client'
+
+const ME = gql`
+  query me {
+    user(login: "keisuke-333") {
+      name
+      avatarUrl
+    }
+  }
+`
+
+type FetchData = {
+  name: string
+  avatarUrl: string
+}
+
 const App = () => {
+  const [userData, setUserData] = useState<FetchData>()
+
+  const fetch = async () => {
+    const { data } = await client.query({
+      query: ME
+    })
+    setUserData(data.user)
+  }
+
+  useEffect(() => {
+    fetch()
+  }, [])
+
   return (
-    <div>
-      テスト
-    </div>
+    <>
+      <p>{userData?.name}</p>
+      <p>{userData?.avatarUrl}</p>
+    </>
   )
 }
 
