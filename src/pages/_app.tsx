1b939505733c3d1e73a7a90c74915a66637c23bb
keisuke-333 <keikubo.999@gmail.com>
2021-12-31 16:45:30 +0900

    Client-side GraphQL queries

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,17 @@
 import type { AppProps } from 'next/app'
+import { ApolloProvider } from '@apollo/client'
 import 'tailwindcss/tailwind.css'
 
+import apolloClient from '../libs/apollo'
 import Layout from '../components/Layout'
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+     <ApolloProvider client={apolloClient}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+     </ApolloProvider>
   )
 }
 export default App
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,28 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import { linkDatas } from '../../prisma/data/linkDatas'
+import { gql, useQuery } from '@apollo/client'
+
 import { AwesomeLink } from '../components/AwesomeLink'
 
+const AllLinksQuery = gql`
+  query {
+    links {
+      id
+      title
+      url
+      description
+      imageUrl
+      category
+    }
+  }
+`
+
 const IndexPage: NextPage = () => {
+   const { data, loading, error } = useQuery(AllLinksQuery)
+
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Oh no... {error.message}</p>
+
   return (
     <>
       <Head>
@@ -12,15 +31,15 @@ const IndexPage: NextPage = () => {
       </Head>
       <div className="container mx-auto max-w-5xl my-20">
         <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
-          {linkDatas.map((data) => (
+          {data?.links.map((link) => (
             <AwesomeLink
-              key={data.id}
-              id={data.id}
-              title={data.title}
-              description={data.description}
-              url={data.url}
-              imageUrl={data.imageUrl}
-              category={data.category}
+              key={link.id}
+              id={link.id}
+              title={link.title}
+              description={link.description}
+              url={link.url}
+              imageUrl={link.imageUrl}
+              category={link.category}
             />
           ))}
         </ul>
