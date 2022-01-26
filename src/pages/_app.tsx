757715cdbe6d2a77572d3e6fb5de8942c3e1fcd6
keisuke-333 <keikubo.999@gmail.com>
2022-01-27 00:31:22 +0900

    Apollo client with Hasura

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,15 @@
 import { AppProps } from 'next/app'
+import { ApolloProvider } from '@apollo/client'
 import 'tailwindcss/tailwind.css'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import apolloClient from '../libs/apolloClient'
+
+const App = ({ Component, pageProps }: AppProps) => {
+  return (
+    <ApolloProvider client={apolloClient}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  )
 }
 
-export default MyApp
+export default App
---
+++ src/pages/hasura-main.tsx
@@ -0,0 +1,48 @@
+import { VFC } from 'react'
+import { NextPage } from 'next'
+import Link from 'next/link'
+import { useQuery } from '@apollo/client'
+
+import { GET_USERS } from '../../queries/queries'
+import { GetUsersQuery } from '../../types/generated/graphql'
+import { Layout } from '../components/Layout'
+
+const HasuraMainPage: NextPage = () => {
+  const { data, loading, error } = useQuery<GetUsersQuery>(GET_USERS)
+
+  if (loading) {
+    return (
+      <Layout title="Hasura fetchPolicy">
+        <p>Loading...</p>
+      </Layout>
+    )
+  }
+
+  if (error) {
+    return (
+      <Layout title="Hasura fetchPolicy">
+        <p>Error: {error.message}</p>
+      </Layout>
+    )
+  }
+
+  return (
+    <Layout title="Hasura fetchPolicy">
+      <p className="mb-6 font-bold">Hasura main page</p>
+      {console.log(data)}
+      {data?.users.map((user) => (
+        <p
+          className="my-1"
+          key={user.id}
+        >
+          {user.name}
+        </p>
+      ))}
+      <Link href="/hasura-sub">
+        <a className="mt-6">Next</a>
+      </Link>
+    </Layout>
+  )
+}
+
+export default HasuraMainPage
