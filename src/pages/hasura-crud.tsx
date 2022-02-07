f34b484363b8bcfb8a8124b8f4a6d237e0f69c17
keisuke-333 <keikubo.999@gmail.com>
2022-02-07 23:04:37 +0900

    一旦途中まで

--- src/pages/hasura-crud.tsx
+++ src/pages/hasura-crud.tsx
@@ -17,6 +17,7 @@ import {
 import { Layout } from '../components/Layout'
 
 const HasuraCrudPage: NextPage = () => {
+  const [editedUser, setEditedUser] = useState({ id: '', name: '' })
   const { data, error } = useQuery<GetUsersQuery>(GET_USERS)
   const [update_users_by_pk] = useMutation<UpdateUserMutation>(UPADE_USER)
   const [insert_users_one] = useMutation<CreateUserMutation>(CREATE_USER, {
@@ -45,9 +46,65 @@ const HasuraCrudPage: NextPage = () => {
     }
   })
 
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    if(editedUser.id) {
+      try {
+        await update_users_by_pk({
+          variables: {
+            id: editedUser.id,
+            name: editedUser.name
+          }
+        })
+      } catch(err) {
+        alert(err.message)
+      }
+      setEditedUser({ id: '', name: ''})
+    } else {
+      try {
+        await insert_users_one({
+          variables: {
+            name: editedUser.name
+          }
+        })
+      } catch(err) {
+        alert(err.message)
+      }
+      setEditedUser({ id: '', name: '' })
+    }
+  }
+
+  if(error) {
+    return (
+      <Layout title="Hasura CRUD">
+        Error: {error.message}
+      </Layout>
+    )
+  }
+
   return (
     <Layout title="Hasura CRUD">
       <p className="mb-3 font-bold">Hasura CRUD</p>
+      <form
+        className="flex flex-col justify-center items-center"
+        onSubmit={handleSubmit}
+      >
+        <input
+          className="px-3 py-2 border border-gray-300"
+          placeholder="New user ?"
+          type="text"
+          value={editedUser.name}
+          onChange={(e) => setEditedUser({...editedUser, name: e.target.value})}
+        />
+        <button
+          disabled={!editedUser.name}
+          className="disabled:opacity-40 my-30 py-1 px-3 text-white bg-indigo-600 hover:bg-indigo-700 rounded-2xl focus:outline-none"
+          data-testid="new"
+          type="submit"
+        >
+          {editedUser.id ? 'Update' : 'Create'}
+        </button>
+      </form>
     </Layout>
   )
 }
