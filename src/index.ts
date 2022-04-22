d008471312e1e3eed9ba96cd9ace046d453a874c
keisuke-333 <keikubo.999@gmail.com>
2022-04-22 13:27:30 +0900

    urlとメソッド指定

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,13 @@ import * as http from 'http'
 import products from './data/products.json'
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'application/json' })
-  res.end(JSON.stringify(products))
+  if (req.url === '/api/products' && req.method === 'GET') {
+    res.writeHead(200, { 'Content-Type': 'application/json' })
+    res.end(JSON.stringify(products))
+  } else {
+    res.writeHead(404, { 'Content-Type': 'application/json' })
+    res.end(JSON.stringify({ message: 'Route Not Found' }))
+  }
 })
 
 const PORT = process.env.PORT || 3000
