04005d117fb4e305a8b5ebfacfc94393d7a9827e
keisuke-333 <keikubo.999@gmail.com>
2022-04-22 12:39:03 +0900

    html表示

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import * as http from 'http'
 
 const server = http.createServer((req, res) => {
-  res.end('Hello!')
+  res.statusCode = 200
+  res.setHeader('Content-Type', 'text/html')
+  res.write('<h1>Hello World</h1>')
+  res.end()
 })
 
 const PORT = process.env.PORT || 3000
