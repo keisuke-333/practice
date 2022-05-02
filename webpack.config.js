9dac22ec92b1d4cb545e6aabb41809c5d92cb453
keisuke-333 <keikubo.999@gmail.com>
2022-05-02 16:54:55 +0900

    sassや画像のバンドル

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,15 @@
 const path = require('path')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+
+const MODE = 'development'
+const sourceMapStatus = MODE === 'development'
 
 module.exports = {
   context: path.join(__dirname, 'src'),
-  entry: ["./index.js", "./sub1.js", "./sub2.js"],
+  entry: "./index.js",
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: "./bundle.js",
+    filename: "main.js",
     clean: {
       keep: /index.html/,
     }
@@ -15,5 +19,44 @@ module.exports = {
   devServer: {
     static: "dist",
     open: true
-  }
+  },
+  module: {
+    rules: [
+      {
+        test:/\.(sass|scss|css)$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader
+          },
+          {
+            loader: "css-loader",
+            options: {
+              url: true,
+              sourceMap: sourceMapStatus
+            }
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: sourceMapStatus
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(gif|png|jpg|svg)$/,
+        type: "asset",
+        parser: {
+          dataUrlCondition: {
+            maxSize: 100 * 1024
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '../css/[name].css'
+    })
+  ]
 }
