097c8818fcf957381fce7ec708ccacb7688d5775
keisuke-333 <keikubo.999@gmail.com>
2022-06-27 19:47:02 +0900

    On-demand ISR invoked by function hooks

--- src/pages/api/revalidate/index.ts
+++ src/pages/api/revalidate/index.ts
@@ -3,9 +3,15 @@ import { NextApiHandler } from 'next'
 type Data = {
   revalidated: boolean
 }
+type Msg = {
+  message: string
+}
 
-const handler: NextApiHandler<Data> = async (req, res) => {
+const handler: NextApiHandler<Data | Msg> = async (req, res) => {
   console.log('Revalidating notes page...')
+  if (req.query.secret !== process.env.REVALIDATE_SECRET) {
+    return res.status(401).json({ message: 'Your secret is invalid !' })
+  }
   let revalidated = false
   try {
     await res.unstable_revalidate('/notes')
