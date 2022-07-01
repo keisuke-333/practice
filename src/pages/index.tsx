41ac17dd6eb7884dba0f862eeb4fd7498f2ed454
keisuke-333 <keikubo.999@gmail.com>
2022-07-02 00:47:27 +0900

    setup project

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const HomePage: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Realtime App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <p>Realtime App</p>
+    </div>
+  )
+}
+
+export default HomePage
