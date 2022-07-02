ee5b034faeee52aaa1c1504fa7b7ab09ba9b7329
keisuke-333 <keikubo.999@gmail.com>
2022-07-02 12:53:04 +0900

    make react-query directory

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,8 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { QueryClient, QueryClientProvider } from 'react-query'
+import { QueryClientProvider } from 'react-query'
+import { queryClient } from 'libs/react-query/queryClient'
 import { ReactQueryDevtools } from 'react-query/devtools'
-
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      retry: false,
-      refetchOnWindowFocus: false,
-      suspense: true,
-    },
-  },
-})
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
