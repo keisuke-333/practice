21ec2d09b6509e45fc49c770c8296fc265eb1a28
keisuke-333 <keikubo.999@gmail.com>
2022-07-14 03:25:31 +0900

    add Notification to DashBoard

--- src/components/DashBoard.tsx
+++ src/components/DashBoard.tsx
@@ -6,13 +6,17 @@ import { supabase } from 'libs/supabase/client'
 import useStore from 'libs/zustand/store'
 import { Spinner } from './Spinner'
 import { UserProfile } from './UserProfile'
+import { Notification } from './Notification'
 
 export const DashBoard: FC = () => {
   const queryClient = useQueryClient()
   const resetProfile = useStore((state) => state.resetEditedProfile)
+  const resetNotice = useStore((state) => state.resetEditedNotice)
   const signOut = () => {
     resetProfile()
+    resetNotice()
     queryClient.removeQueries(['profile'])
+    queryClient.removeQueries(['notices'])
     supabase.auth.signOut()
   }
   return (
@@ -21,16 +25,29 @@ export const DashBoard: FC = () => {
         className="my-6 h-6 w-6 cursor-pointer text-blue-500"
         onClick={signOut}
       />
-      <div className="flex flex-col items-center">
-        <ErrorBoundary
-          fallback={
-            <ExclamationCircleIcon className="my-5 h-10 w-10 text-pink-500" />
-          }
-        >
-          <Suspense fallback={<Spinner />}>
-            <UserProfile />
-          </Suspense>
-        </ErrorBoundary>
+      <div className="grid grid-cols-2 gap-4">
+        <div className="flex flex-col items-center">
+          <ErrorBoundary
+            fallback={
+              <ExclamationCircleIcon className="my-5 h-10 w-10 text-pink-500" />
+            }
+          >
+            <Suspense fallback={<Spinner />}>
+              <UserProfile />
+            </Suspense>
+          </ErrorBoundary>
+        </div>
+        <div className="flex flex-col items-center">
+          <ErrorBoundary
+            fallback={
+              <ExclamationCircleIcon className="my-5 h-10 w-10 text-pink-500" />
+            }
+          >
+            <Suspense fallback={<Spinner />}>
+              <Notification />
+            </Suspense>
+          </ErrorBoundary>
+        </div>
       </div>
     </>
   )
